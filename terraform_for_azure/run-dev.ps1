b2b2f87608e53b1746a2f680f73a9b7467d69fe2
Aurelia Bălășoiu <aureliabalasoiu@gmail.com>
2025-04-01 16:49:01 +0300

    Update PS script to prevent 'Backend changed configuration' error

--- terraform_for_azure/run-dev.ps1
+++ terraform_for_azure/run-dev.ps1
@@ -3,13 +3,18 @@
 $env:ARM_SUBSCRIPTION_ID = "your-subscription-id"
 $env:ARM_TENANT_ID = "your-tenant-id"
 $env:ARM_CLIENT_ID = "your-service-principal-id"
-$env:ARM_CLIENT_SECRET = "ypur-client-secret"
+$env:ARM_CLIENT_SECRET = "your-client-secret"
 
 
 # Initialize Terraform with backend config
-terraform init -reconfigure -backend-config="./backend/backend.dev.config"
+terraform init -backend-config="./backend/backend.dev.config"
+
 
 # Validate and apply
 terraform validate
-terraform plan -var-file="./env/dev.tfvars"
 terraform apply -var-file="./env/dev.tfvars" -auto-approve
+
+
+# Even with a remote backend, .terraform/ is required locally for Terraform to work, so it gets generated.
+# To avoid Backend configuration changed error, the .terraform directory is removed
+Remove-Item -Path ".terraform" -Recurse -Force
