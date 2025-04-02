2891bada49a230c0df513a5a89d50543020ac859
Aurelia Bălășoiu <aureliabalasoiu@gmail.com>
2025-04-02 13:11:31 +0300

    Updated ps script to retrieve Terraform credentials from Azure Key vault

--- terraform_for_azure/run-dev.ps1
+++ terraform_for_azure/run-dev.ps1
@@ -1,10 +1,26 @@
+# install the Az.KeyVault module(to be able to use Get-AzKeyVaultSecret) if it does not exist
+if (-not (Get-Module -ListAvailable -Name Az.KeyVault)) {
+    Write-Host "Az.KeyVault not installed. Installing..."
+    Install-Module -Name Az.KeyVault -Scope CurrentUser -Force -AllowClobber
+} else {
+    Write-Host "Az.KeyVault is already installed."
+}
+
+$vaultName = "kv-common101"
+$subscriptionKey = "subscriptionId"
+$tenantKey = "tenantId"
+$terraformServicePrincipalKey = "terraformServicePrincipalId"
+$terraformServicePrincipalSecretKey = "terraformServicePrincipalSecret"
+
 
 # Set environment variables for Azure authentication & terraform backend setup
-$env:ARM_SUBSCRIPTION_ID = "your-subscription-id"
-$env:ARM_TENANT_ID = "your-tenant-id"
-$env:ARM_CLIENT_ID = "your-service-principal-id"
-$env:ARM_CLIENT_SECRET = "your-client-secret"
+$env:ARM_SUBSCRIPTION_ID = Get-AzKeyVaultSecret -VaultName $vaultName -Name $subscriptionKey -AsPlainText
+$env:ARM_TENANT_ID = Get-AzKeyVaultSecret -VaultName $vaultName -Name $tenantKey -AsPlainText
+$env:ARM_CLIENT_ID = Get-AzKeyVaultSecret -VaultName $vaultName -Name $terraformServicePrincipalKey -AsPlainText
+$env:ARM_CLIENT_SECRET = Get-AzKeyVaultSecret -VaultName $vaultName -Name $terraformServicePrincipalSecretKey -AsPlainText
 
+
+Write-Host "Subscription ID from Key Vault: $env:ARM_SUBSCRIPTION_ID"
 
 # Initialize Terraform with backend config
 terraform init -backend-config="./backend/backend.dev.config"
